af8feb79a8c29fd086cd30148eefc02f10338175
Shiva Rawat <113193651+shivarawat5630@users.noreply.github.com>
2022-12-08 17:16:14 +0530

    Add files via upload

---
+++ changable desktop by javascript/script.js
@@ -0,0 +1,38 @@
+function red(){
+    document.getElementById("p1").style.backgroundColor="red";
+}
+function green(){
+    document.getElementById("p1").style.backgroundColor="green";
+}
+function yellow(){
+    document.getElementById("p1").style.backgroundColor="yellow";
+}
+function brown(){
+    document.getElementById("p1").style.backgroundColor="brown";
+}
+function indigo(){
+    document.getElementById("p1").style.backgroundColor="indigo";
+}
+function orange(){
+    document.getElementById("p1").style.backgroundColor="orange";
+}
+function white(){
+    document.getElementById("p1").style.backgroundColor="white";
+}
+function black(){
+    document.getElementById("p1").style.backgroundColor="black";
+}
+function blue(){
+    document.getElementById("p1").style.backgroundColor="blue";
+}
+function violet(){
+    document.getElementById("p1").style.backgroundColor="violet";
+}
+function myfunction(){
+    document.getElementById("date").innerHTML=Date();
+    document.getElementById("date").style.backgroundColor="aqua";
+
+
+
+
+}
